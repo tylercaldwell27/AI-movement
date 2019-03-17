e09fca8d2a12f04f23d2abac67cd18c65b7f79cb
tylercaldwell27 <44902413+tylercaldwell27@users.noreply.github.com>
2019-03-17 13:30:18 -0400

    Create AiMovement.cs

---
+++ AiMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+public class AiMovement : MonoBehaviour
+{
+    public Transform target;
+    public Transform myTransform;
+    public float distanceAway;
+    private NavMeshAgent navCompnent;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        navCompnent = this.gameObject.GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+
+     
+            navCompnent.SetDestination(target.position);
+        
+
+        
+    }
+
+   
+ 
+    }
